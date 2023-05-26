70a3c7cd38cf3fa2595d9b33b685cbc205bca6d5
Benjamin <benjaminviola@outlook.es>
2023-05-26 18:02:03 -0400

    some adjustments to the database connection and the readme

--- backend/src/db/connect.js
+++ backend/src/db/connect.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
+import {config} from 'dotenv'
+config();
 
 
 const connectDB = async () => {
     mongoose.set('strictQuery', true);
 
-   const db = await mongoose.connect('mongodb://127.0.0.1/imgApp');
+   const db = await mongoose.connect(process.env.MONGODB_URI);
 
    if (db) {
     console.log('connected to database');
